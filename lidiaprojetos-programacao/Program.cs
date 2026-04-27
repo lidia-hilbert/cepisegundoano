143742969eeff662c5c4b10b4a3bd58d7800e3eb
lidia-hilbert <ohilbert.lidia@gmail.com>
2026-04-27 11:27:55 -0300

    Add files via upload

---
+++ lidiaprojetos-programacao/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Atividade_do_Anteenilsom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Quantos numeros inteiros você quer somar: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int soma = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                //Console.WritLine("oi);
+                Console.WriteLine("Valor : #{0}", i);
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+            {
+            Console.WriteLine("soma = " + soma);
+
+
+
+
+            }
+      }
+ }
+    
+
